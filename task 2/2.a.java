f9e152dfbf1045b1495f499b2088602532719f74
vtu21562 <vtu21562@veltech.edu.in>
2025-09-17 11:27:29 +0530

    Update and rename TASK-2 to task 2/2.a.java

---
+++ task 2/2.a.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class task2a {
+public static int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+List<int[]> result = new ArrayList<>();
+int i = 0, j = 0;
+while (i < firstList.length && j < secondList.length) {
+int start = Math.max(firstList[i][0], secondList[j][0]);
+int end = Math.min(firstList[i][1], secondList[j][1]);
+if (start <= end) {
+result.add(new int[]{start, end});
+}
+if (firstList[i][1] < secondList[j][1]) {
+i++;
+} else {
+j++;
+}
+}
+return result.toArray(new int[result.size()][]);
+}
+public static void main(String[] args) {
+int[][] firstList = {{1, 3}, {5, 9}};
+int[][] secondList = {{2, 5}, {7, 10}};
+int[][] intersections = intervalIntersection(firstList, secondList);
+System.out.println("Intersections:");
+for (int[] interval : intersections) {
+System.out.println("[" + interval[0] + ", " + interval[1] + "]");
+}
+}
+}
