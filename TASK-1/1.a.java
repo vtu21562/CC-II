2eae7fd9bdd9c995ea103cdf5cb6642ecba0bcb3
vtu21562 <vtu21562@veltech.edu.in>
2025-09-17 11:04:49 +0530

    Create 1.a.java

---
+++ TASK-1/1.a.java
@@ -0,0 +1,15 @@
+import  java.util.*;
+class task1a{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int i=1;
+        while(i*i <=n){
+            i++;
+
+        }
+        System.out.println(i-1);
+        
+    }
+
+}
